ec168be6a133bbcc88c9bd5b65361d8e05367ed5
eboissier <eboissier@e07a6ad9-7c28-0410-a8f0-b6e01509c4f6>
2014-07-11 15:02:54 +0000

    git-svn-id: https://gateway.terradue.com/repo/projects/internal/t2portal/trunk@36977 e07a6ad9-7c28-0410-a8f0-b6e01509c4f6

---
+++ src/main/scripts/production-install.sh
@@ -0,0 +1,44 @@
+#! /bin/sh
+
+#configuration
+portal_path="/usr/local/t2portal/webserver"
+site="t2portal"
+db="t2portal"
+dump="/tmp/t2portal-db-dump.sql"
+dbuser="root"
+publicIP="http://www.terradue.com"
+
+mkdir -p $portal_path/services
+mkdir -p $portal_path/modules
+
+mkdir /var/www/.config
+chown apache:apache /var/www/.config
+
+#link services
+mkdir -p $portal_path/sites/$site/root/services
+mkdir -p $portal_path/sites/$site/root/files
+chown apache:apache $portal_path/sites/$site/root/files
+
+#dynamic hostname
+sed -i -e 's/${PORTALWEBSERVER}/'$HOSTNAME'/g' $portal_path/sites/$site/root/web.config
+sed -i -e 's/${PORTALWEBSERVER}/'$HOSTNAME'/g' $portal_path/sites/$site/config/*
+sed -i -e 's/${PORTALWEBSERVER}/'$HOSTNAME'/g' /etc/httpd/conf.d/*t2portal*
+
+#dumped database copy
+if [ -f $dump ] 
+then 
+    echo "Copy of the dumped database \`$db\`"
+	mysql -u root -e "drop database if exists \`$db\`"
+	mysql -u root -e "create database \`$db\`"
+	mysql -u root -e "grant all on \`$db\`.* to 'portal'@'localhost' IDENTIFIED by 'portal'"
+	mysql -u root $db < $dump
+else
+	echo "No copy of the dumped database $db"
+	mysql -u root -e "grant all on \`$db\`.* to 'portal'@'localhost' IDENTIFIED by 'portal'" 
+fi
+
+#mono and mysql
+mono $portal_path/sites/$site/root/bin/Terradue.Portal.AdminTool.exe auto -r $portal_path/sites/$site/root -u root -S $db
+
+chkconfig --add t2portal-agent
+
