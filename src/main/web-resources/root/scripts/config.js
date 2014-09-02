6275d35ef069bd862e5e42f8c4b38cf9261d0ca9
hcaumont <hcaumont@e07a6ad9-7c28-0410-a8f0-b6e01509c4f6>
2014-09-02 07:15:03 +0000

    Changes on www-root: - added a new index page, with js menu (unit tested, but not working on the Portal env) - added 2 test pages tour.html and resources.html - added image files under pages - added 'new-home' folder with my js and css under 'utils'
    git-svn-id: https://gateway.terradue.com/repo/projects/internal/t2portal/trunk@37559 e07a6ad9-7c28-0410-a8f0-b6e01509c4f6

--- src/main/web-resources/root/scripts/config.js
+++ src/main/web-resources/root/scripts/config.js
@@ -10,20 +10,8 @@ define({
 	
 	staticPages: {
 		'about': '/static/about.html',
-		'boh': '/boh.html',
-		'contact': {
-			url: '/static/contact.html',
-			data: [
-				{id:1, name: 'pinco', surname: 'pallino'},
-				{id:2, name: 'tizio', surname: 'caio'},
-				{id:3, name: 'sempronio', surname: 'bah'},
-				{id:4, name: 'ciccio', surname: 'ceras', username:'ciccio'},
-			],
-			fnLoad: function(){
-				// alert('loaded');
-			}
-		},
-		'contact/ciccio': '/static/contact/ciccio.html',
+		'tour': '/tour.html',
+		'resources': '/resources.html',
 	},
 	
 });
