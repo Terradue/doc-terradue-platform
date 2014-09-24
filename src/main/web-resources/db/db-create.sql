b2f4b0bb3d25c805e9349d572faba02de2bc39ba
eboissier <eboissier@e07a6ad9-7c28-0410-a8f0-b6e01509c4f6>
2014-09-24 12:55:12 +0000

    git-svn-id: https://gateway.terradue.com/repo/projects/internal/t2portal/trunk@38178 e07a6ad9-7c28-0410-a8f0-b6e01509c4f6

--- src/main/web-resources/db/db-create.sql
+++ src/main/web-resources/db/db-create.sql
@@ -6,17 +6,6 @@ USE $MAIN$;
 
 /*****************************************************************************/
 
--- Creating usr_github table
-CREATE TABLE usr_github (
-    id_usr int unsigned NOT NULL COMMENT 'FK: User',
-    username varchar(50) COMMENT 'Username on github',
-    CONSTRAINT pk_usrgithub PRIMARY KEY (id_usr),
-    CONSTRAINT fk_usrgithub_usr FOREIGN KEY (id_usr) REFERENCES usr(id) ON DELETE CASCADE
-) Engine=InnoDB COMMENT 'User github';
--- RESULT
-
-/*****************************************************************************/
-
 -- Adding privileges for data packages ... \
 SET @priv_pos = (SELECT MAX(pos) FROM priv);
 INSERT INTO priv (id_type, operation, pos, name) VALUES
@@ -33,16 +22,6 @@ UPDATE type SET custom_class = 'Terradue.Corporate.Controller.UserT2, Terradue.C
 
 /*****************************************************************************/
 
-INSERT IGNORE INTO usrcert (id_usr) SELECT id from usr;
-
-/*****************************************************************************/
-
 UPDATE type SET keyword='wps' WHERE class='Terradue.Portal.WpsProcessOffering, Terradue.Portal';
 
 /*****************************************************************************/
-
-INSERT INTO config (`name`, `type`, `caption`, `hint`, `value`, `optional`) VALUES ('One-admin-usr', 'string', 'OpenNebula admin username', 'Enter the value of the Opennebula admin username', 'portal', '0');
-INSERT INTO config (`name`, `type`, `caption`, `hint`, `value`, `optional`) VALUES ('One-admin-pwd', 'string', 'OpenNebula admin password', 'Enter the value of the Opennebula admin password', 'portaltest', '0');
-
-/*****************************************************************************/
-
