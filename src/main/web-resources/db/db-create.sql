171797a8a3c35f5b7b152a8e0c4e13a88542c9e8
eboissier <eboissier@e07a6ad9-7c28-0410-a8f0-b6e01509c4f6>
2014-09-25 13:51:11 +0000

    git-svn-id: https://gateway.terradue.com/repo/projects/internal/t2portal/trunk@38201 e07a6ad9-7c28-0410-a8f0-b6e01509c4f6

--- src/main/web-resources/db/db-create.sql
+++ src/main/web-resources/db/db-create.sql
@@ -25,3 +25,8 @@ UPDATE type SET custom_class = 'Terradue.Corporate.Controller.UserT2, Terradue.C
 UPDATE type SET keyword='wps' WHERE class='Terradue.Portal.WpsProcessOffering, Terradue.Portal';
 
 /*****************************************************************************/
+
+INSERT INTO config (`name`, `type`, `caption`, `hint`, `value`, `optional`) VALUES ('One-xmlrpc-url', 'string', 'OpenNebula XML RPC proxy url', 'Enter the value of the Opennebula XML RPC proxy server url', 'http://cloud-dev.terradue.int:2633/RPC2', '0');
+INSERT INTO config (`name`, `type`, `caption`, `hint`, `value`, `optional`) VALUES ('One-admin-usr', 'string', 'OpenNebula admin username', 'Enter the value of the Opennebula admin username', 'serveradmin', '0');
+INSERT INTO config (`name`, `type`, `caption`, `hint`, `value`, `optional`) VALUES ('One-admin-pwd', 'string', 'OpenNebula admin password', 'Enter the value of the Opennebula admin password', 'f4b887a18de059129df8a265176f80bc479439a6', '0');
+INSERT INTO config (`name`, `type`, `caption`, `hint`, `value`, `optional`) VALUES ('One-access', 'string', 'OpenNebula access url', 'Enter the value of the Opennebula access url', 'https://cloud-dev.terradue.int', '0');
