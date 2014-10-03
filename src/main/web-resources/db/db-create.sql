da5b8977796a5a0f05c6ad7ae49907066b1186ec
eboissier <eboissier@e07a6ad9-7c28-0410-a8f0-b6e01509c4f6>
2014-10-03 13:22:25 +0000

    git-svn-id: https://gateway.terradue.com/repo/projects/internal/t2portal/trunk@38349 e07a6ad9-7c28-0410-a8f0-b6e01509c4f6

--- src/main/web-resources/db/db-create.sql
+++ src/main/web-resources/db/db-create.sql
@@ -4,6 +4,21 @@ USE $MAIN$;
 
 -- Initializing t2portal data model ... \ 
 
+/*****************************************************************************/
+
+UPDATE type SET custom_class = 'Terradue.Corporate.Controller.UserT2, Terradue.Corporate.Controller' WHERE class = 'Terradue.Portal.User, Terradue.Portal';
+
+/*****************************************************************************/
+
+INSERT IGNORE INTO usrcert (id_usr) SELECT id from usr;
+
+/*****************************************************************************/
+
+INSERT INTO config (`name`, `type`, `caption`, `hint`, `value`, `optional`) VALUES ('One-xmlrpc-url', 'string', 'OpenNebula XML RPC proxy url', 'Enter the value of the Opennebula XML RPC proxy server url', 'http://cloud-dev.terradue.int:2633/RPC2', '0');
+INSERT INTO config (`name`, `type`, `caption`, `hint`, `value`, `optional`) VALUES ('One-admin-usr', 'string', 'OpenNebula admin username', 'Enter the value of the Opennebula admin username', 'serveradmin', '0');
+INSERT INTO config (`name`, `type`, `caption`, `hint`, `value`, `optional`) VALUES ('One-admin-pwd', 'string', 'OpenNebula admin password', 'Enter the value of the Opennebula admin password', 'f4b887a18de059129df8a265176f80bc479439a6', '0');
+INSERT INTO config (`name`, `type`, `caption`, `hint`, `value`, `optional`) VALUES ('One-access', 'string', 'OpenNebula access url', 'Enter the value of the Opennebula access url', 'https://cloud-dev.terradue.int', '0');
+
 UPDATE config SET value='terradue.com' WHERE name='Github-client-name';
 UPDATE config SET value='64e9f7050a5dba093679' WHERE name='Github-client-id';
 UPDATE config SET value='3c84f555347681b0e0635ffbcbfb3cbbb3504b5e' WHERE name='Github-client-secret';
