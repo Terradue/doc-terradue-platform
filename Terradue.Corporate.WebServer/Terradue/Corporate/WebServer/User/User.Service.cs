54e170dd9ab6762a316e84c72d4f648024b19ee4
eboissier <enguerran.boissier@terradue.com>
2016-02-17 09:54:38 +0100

    fill user email

--- Terradue.Corporate.WebServer/Terradue/Corporate/WebServer/User/User.Service.cs
+++ Terradue.Corporate.WebServer/Terradue/Corporate/WebServer/User/User.Service.cs
@@ -179,6 +179,7 @@ namespace Terradue.Corporate.WebServer {
 
                 UserT2 user = request.ToEntity(context, new UserT2(context));
                 user.Username = user.Email;
+                user.Email = user.Email;
                 user.NeedsEmailConfirmation = false;
                 user.AccountStatus = AccountStatusType.PendingActivation;
                 user.Level = UserLevel.User;
