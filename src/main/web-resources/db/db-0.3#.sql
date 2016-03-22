9afe36c8d9e7e904935857f1a2eac8dfd518254b
eboissier <enguerran.boissier@terradue.com>
2016-03-22 17:21:43 +0100

    up db create

--- src/main/web-resources/db/db-0.3#.sql
+++
@@ -1,9 +0,0 @@
-USE $MAIN$;
-
-/*****************************************************************************/
-
--- Adding config... \
-INSERT INTO config (`name`, `type`, `caption`, `hint`, `value`, `optional`) VALUES ('t2-safe-token', 'string', 'secret token to get ssh pubkey', 'secret token to get ssh pubkey', 'jfiojsealhjfkdn4kjfdskjnfjhskjnfjklhjmEDFFSR23433fdf', '0');
--- RESULT
-
-/*****************************************************************************/
