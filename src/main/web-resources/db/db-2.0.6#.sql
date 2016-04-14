b2378fbc9f78a2c616aa31ee8cce4b3d0adfcf9e
eboissier <enguerran.boissier@terradue.com>
2016-04-14 11:18:06 +0200

    add zendesk

--- src/main/web-resources/db/db-2.0.6#.sql
+++ src/main/web-resources/db/db-2.0.6#.sql
@@ -4,8 +4,12 @@ USE $MAIN$;
 
 -- Adding discourse SSO ... \
 INSERT INTO config (name, id_section, pos, internal, type, caption, hint, value, optional) VALUES ('discourse-sso-secret', NULL, NULL, '0', 'string', 'discourse sso secret', 'discourse sso secret', 'T3rr4du3Discours3', '1');
-INSERT INTO config (name, id_section, pos, internal, type, caption, hint, value, optional) VALUES ('discourse-sso-callback', NULL, NULL, '0', 'string', 'discourse sso callback', 'discourse sso callback', 'http://discuss.terradue.com/session/sso_login', '1');
+INSERT INTO config (name, id_section, pos, internal, type, caption, hint, value, optional) VALUES ('discourse-sso-callback', NULL, NULL, '0', 'string', 'discourse sso callback', 'discourse sso callback', 'https://discuss.terradue.com/session/sso_login', '1');
+-- RESULT
 
+-- Adding zendesk SSO ... \
+INSERT INTO config (name, id_section, pos, internal, type, caption, hint, value, optional) VALUES ('zendesk-sso-secret', NULL, NULL, '0', 'string', 'zendesk sso secret', 'zendesk sso secret', '5Gjz1rGjFlPRcqrFXUAOfCA9kRJamuoK0xvu745eNuIl9ItJ', '1');
+INSERT INTO `t2portal`.`config` (`name`, `id_section`, `pos`, `internal`, `type`, `caption`, `hint`, `value`, `optional`) VALUES ('zendesk-sso-callback', NULL, NULL, '0', 'string', 'zendesk sso callback', 'zendesk sso callback', 'https://terradue.zendesk.com/access/jwt', '1');
 -- RESULT
 
 /*****************************************************************************/
