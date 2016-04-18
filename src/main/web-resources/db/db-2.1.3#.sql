e767bd8b1d397a94c4895342a582a84ab7cb7497
eboissier <enguerran.boissier@terradue.com>
2016-04-18 17:08:28 +0200

    up db

---
+++ src/main/web-resources/db/db-2.1.3#.sql
@@ -0,0 +1,6 @@
+USE $MAIN$;
+/*****************************************************************************/
+
+-- CONFIG
+INSERT INTO config (name, id_section, pos, internal, type, caption, hint, value, optional) VALUES ('t2portal-token-usrsso', NULL, NULL, '0', 'string', 'T2portal sso token', 'Enter the t2portal sso token', 'd0fa00c2-91bd-4df8-b96c-662117230c79', '1');
+-- RESULT
