61b338b811646204cd796abb7e98304b672d4042
eboissier <enguerran.boissier@terradue.com>
2016-10-21 08:45:53 +0200

    sql for store

---
+++ src/main/web-resources/db/db-2.3#.sql
@@ -0,0 +1,7 @@
+USE $MAIN$;
+/*****************************************************************************/
+
+-- CONFIG
+UPDATE config SET value='https://store.terradue.com/sapi' WHERE name='artifactory-APIurl';
+-- RESULT
+
