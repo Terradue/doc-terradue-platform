d29d374a8d82412544630121f3125c0b33589b59
eboissier <enguerran.boissier@terradue.com>
2017-05-16 09:27:24 +0200

    add no plan

--- src/main/web-resources/db/db-2.3.2#.sql
+++ src/main/web-resources/db/db-2.3.2#.sql
@@ -7,4 +7,8 @@ ALTER TABLE usr
 ADD COLUMN links TEXT NULL DEFAULT NULL;
 -- RESULT
 
+-- Add no plan role
+INSERT INTO role (identifier, name, description) VALUES ('plan_NoPlan', 'No Plan', 'no plan user');
+-- RESULT
+
 /*****************************************************************************/
